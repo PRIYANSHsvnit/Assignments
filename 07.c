d6326ef0960648906a8ff18c9b0f74376389212a
PRIYANSHsvnit <u24ai048@aid.svnit.ac.in>
2024-09-02 21:08:17 +0530

    Update 07.c

--- 07.c
+++ 07.c
@@ -1,11 +1,11 @@
 #include<stdio.h>
 int main()
 {
-int a,b,c,s,Ar2;
+float a,b,c,s,Ar2;
 printf("Enter the lenght of sides in metre:");
-scanf("%d%d%d",&a,&b,&c);
+scanf("%f%f%f",&a,&b,&c);
 s=(a+b+c)/2;
-printf("Semi perimetre = %d",s);
+printf("Semi perimetre = %f",s);
 Ar2=s*(s-a)*(s-b)*(s-c);
-printf("Area's Square in sq. metre = %d", Ar2);
+printf("Area's Square in sq. metre = %f", Ar2);
 }
